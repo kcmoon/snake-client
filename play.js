101ff9b8b24f229af3b89c1b0763f18c0f9b379d
Kenny Moon <kennymoon90@gmail.com>
2022-06-08 16:34:40 -0700

    refactored and added requires to input.js

--- play.js
+++ play.js
@@ -1,20 +1,8 @@
 const { stdin } = require('process');
 const {connect} = require('./client.js');
+const {setupInput} = require('./input');
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', (key) => handleUserInput(key));
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    console.log('terminating program')
-    process.exit();
-  };
-};
+console.log('Connecting...');
+connect();
 
 setupInput();
