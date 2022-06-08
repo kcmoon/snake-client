42b2530ecc46bf0b57d955ac98552f7771df4040
Kenny Moon <kennymoon90@gmail.com>
2022-06-08 10:38:37 -0700

    moved connect function to this file and exported to function using short hand syntax in the object

---
+++ client.js
@@ -0,0 +1,29 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("You are now connected.")
+  });
+
+  conn.on("data", () => {
+    console.log('You dead! You idled.')
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = {
+connect
+};
